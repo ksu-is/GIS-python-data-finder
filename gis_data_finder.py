31b61430e46335127ab8495da5d61dc36e79b1b9
almccleary <49005205+almccleary@users.noreply.github.com>
2019-04-29 17:27:02 -0400

    Add files via upload
    main program file

---
+++ gis_data_finder.py
@@ -0,0 +1,5 @@
+# make sure before you try to run this code, you open ArcMap
+# and load this in the python window!
+#set the map document we're working on
+mxd = arcpy.mapping.MapDocument("CURRENT")
+
