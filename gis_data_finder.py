2f7328c3afd6cccff7dfa5e613cf63c5470fd27f
almccleary <49005205+almccleary@users.noreply.github.com>
2019-05-01 11:02:28 -0400

    Update gis_data_finder.py
    FINISHED creating layers from selected attributes in arcmap, untested

--- gis_data_finder.py
+++ gis_data_finder.py
@@ -1,8 +1,13 @@
 #set the map document we're working on
 mxd = arcpy.mapping.MapDocument(r"C:\Users\almccleary\Documents\IS3020\McCleary_CrimeDensity.mxd")
-#set the dataframe and layer we're searching in
+#set the dataframe and layer we're searching in. 2nd parameter in df is the dataframe you want to work in, enter empty string
+#for default df; 2nd parameter in lyr is the layer you want to work in. Case sensitive!
 df = arcpy.mapping.ListDataFrames(mxd, "Saint Louis")[0]   
 lyr = arcpy.mapping.ListLayers(mxd, "countycrime", df)[0]
+
+#create feature layer (temp layer) to copy to; 1st parameter input layer, 2nd parameter output layer
+arcpy.MakeFeatureLayer_management("countycrime.shp", "Rape_lyr")
 #select by attribute
 arcpy.SelectLayerByAttribute_management(lyr, "NEW_SELECTION", "UCR_CRIME_ = 111')
-#selection is made, time to create layer from selection
+#selection is made, time to copy to new layer from selection
+arcpy.CopyFeatures_management("Rape_lyr", "C:/Users/almccleary/Documents/IS3020/McCleary_Crime.gdb/rape_stats")
